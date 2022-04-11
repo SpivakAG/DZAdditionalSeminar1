e118e70fc85665fcbca999ad372da14e015a77eb
ArtemSpivak <spivak25@yandex.ru>
2022-04-11 23:07:16 +0300

    Initial commit

--- Задача 1/Program.cs
+++ Задача 1/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.WriteLine("Введите число 1");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (numberA > numberB)
+{
+    max = numberA;
+    Console.Write("max=");
+    Console.Write(max);
+}
+else 
+{
+    max = numberB;
+    Console.Write("max=");
+    Console.Write(max);
+}
