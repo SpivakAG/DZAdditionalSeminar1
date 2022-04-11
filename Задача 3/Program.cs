3c6cd130601f74e5fbb8cd58d5d1f15c7e8de47b
ArtemSpivak <spivak25@yandex.ru>
2022-04-11 23:52:22 +0300

    Решена задача 3

---
+++ Задача 3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+double number = Convert.ToDouble(Console.ReadLine());
+double result = number % 2;
+if (result == 0)
+{
+    Console.WriteLine("Да, число четное");
+}
+else
+{
+    Console.WriteLine("Нет, число не четное");
+}
