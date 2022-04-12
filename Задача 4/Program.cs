1f91265250fa789fa4098c05a5f6e545cf825d3b
ArtemSpivak <spivak25@yandex.ru>
2022-04-12 18:44:45 +0300

    решена задача 4

---
+++ Задача 4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+        int number = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+while (count <= number)
+{
+Console.Write(count);
+Console.Write(" ");
+count +=2;
+}
